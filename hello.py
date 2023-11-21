c1736b6b26ba6b29dc9f9307fbcac6fc77b83c98
JR | Front End Developer <jasurackerman2004@gmail.com>
2023-11-21 19:01:14 +0500

    Treasure Island Game

--- hello.py
+++ hello.py
@@ -1,27 +1,17 @@
-print("True Love calculator")
-name1 = input("What's your name? ")
-name2 = input("What's her name? ")
 
-combined_string = name1 + name2
-lowercase_string = combined_string.lower()
-
-t = lowercase_string.count("t")
-r = lowercase_string.count("r")
-u = lowercase_string.count("u")
-e = lowercase_string.count("e")
-
-true = t + r + u + e
-
-l = lowercase_string.count("l")
-o = lowercase_string.count("o")
-v = lowercase_string.count("v")
-e = lowercase_string.count("e")
-love = l + o + v + e
-
-love_score = int(str(true) + str(love))
-if (love_score < 10) or (love_score > 90):
-  print(f"Your love score is {love_score}, you go together like cokes and mentos")
-elif(love_score > 40 and love_score < 50):
-  print(f"Your score is {love_score}, you are alright together")
+print("Welcome to Treasure Island Game!!!")
+left_right = input("Choose left or right: ")
+if left_right == "left":
+  swim_wait = input("Choose swim or wait: ")
+  if swim_wait == "wait":
+    door = input("Which door? red, yellow, blue ")
+    if door == "red":
+      print("Game Over")
+    elif door == "yellow":
+      print("You Win!")
+    elif door == "blue":
+      print("Game Over")
+  else:
+    print("Game Over")
 else:
-  print(f"Your score is {love_score}")
+  print("Game Over")
