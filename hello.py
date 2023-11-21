617c2db6107099a3bed6a72b649f8a22b5fd128f
JR | Front End Developer <jasurackerman2004@gmail.com>
2023-11-21 17:22:10 +0500

    Disney Land

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-percent = int(input("What percentage tip would you like to give? 10,12 or 15? "))
-people = int(input("How many people to split the bill? "))
+print("Welcome to the Disney Park")
+height = int(input("What's your height in cm? "))
+bill = 0
 
-formula = (bill * (percent / 100 + 1)) / people
-
-final = "{:.2f}".format(formula)
-
-print(f"Each person should pay: $ {final}")
+if height > 150:
+  print("You can get into this park!")
+  age = int(input("What's your age? "))
+  if age < 12:
+    bill = 3
+    print("Child ticket is 3$")
+  elif age < 18:
+    bill = 5
+    print("Youth tickets are 5$")
+  else:
+    bill = 7
+    print("Adult tickets are 7$")
+  print("The photo is 3$")
+  want_photo = input("Do you wanna photo to memorise? Type Y or N")
+  if want_photo == "Y":
+    bill += 3
+    print(f"Your total bill is {bill}")
+else:
+  print("Sorry, we can't to access you")
