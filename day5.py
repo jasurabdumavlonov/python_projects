7ff5449a1cbb4ce5766279d65d1f39e2654a1a00
JR | Front End Developer <jasurackerman2004@gmail.com>
2023-11-22 18:31:29 +0500

    High Score Project

--- day5.py
+++ day5.py
@@ -2,14 +2,8 @@ student_heights = input("Input a list of students height ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 print(student_heights)
-
-total_height = 0
-for height in student_heights:
-    total_height += height
-
-number_of_students = 0
-for student in student_heights:
-    number_of_students += 1
-
-average = total_height / number_of_students
-print(round(average))
+highest_score = 0
+for score in student_heights:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
