16391a905d7cc13b3b10a4b7e6882556f5ff6843
JR | Front End Developer <jasurackerman2004@gmail.com>
2023-11-22 20:26:36 +0500

    Adding even number Project

--- day5.py
+++ day5.py
@@ -1,9 +1,16 @@
-student_heights = input("Input a list of students height ").split()
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
-print(student_heights)
-highest_score = 0
-for score in student_heights:
-    if score > highest_score:
-        highest_score = score
-print(f"The highest score in the class is: {highest_score}")
+# First version
+
+total = 0
+
+for i in range(0,101,2):
+  total += i
+print(total)
+
+
+# Second version
+total2 = 0
+
+for number in range(1,101):
+  if number % 2 == 0:
+    total2 += number
+print(total2)
