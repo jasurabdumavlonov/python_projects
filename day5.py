55607a3728be28fdaa6424807bbba1c982af2a77
JR | Front End Developer <jasurackerman2004@gmail.com>
2023-11-22 20:33:05 +0500

    The FizzBuzz game Project

--- day5.py
+++ day5.py
@@ -1,16 +1,12 @@
-# First version
-
-total = 0
-
-for i in range(0,101,2):
-  total += i
-print(total)
 
 
-# Second version
-total2 = 0
+for i in range(1, 101):
+  if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  elif i % 5 == 0:
+    print("Buzz")
+  else:
+    print(i)
 
-for number in range(1,101):
-  if number % 2 == 0:
-    total2 += number
-print(total2)
